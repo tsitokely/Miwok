99f75d174e34b45fed9ada3b54a61a153df9b230
Tsitohaina Rakotondramasy <tsitohaina.toetra.rak@gmail.com>
2019-12-29 01:17:39 +0300

    finalizing the app functionalities

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -16,13 +16,34 @@
 package com.example.android.miwok;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_colors);
+        setContentView(R.layout.word_list);
+
+        //Creating arrayList of Strings
+        ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("red","weṭeṭṭi"));
+        words.add(new Word("green","chokokki"));
+        words.add(new Word("brown","ṭakaakki"));
+        words.add(new Word("gray","ṭopoppi"));
+        words.add(new Word("black","kululli"));
+        words.add(new Word("white","kelelli"));
+        words.add(new Word("dusty yellow","ṭopiisә"));
+        words.add(new Word("mustard yellow","chiwiiṭә"));
+
+        WordAdapter itemsAdapter = new WordAdapter(this, words);
+        ListView listview = (ListView) findViewById(R.id.list);
+
+        listview.setAdapter(itemsAdapter);
+
     }
 }
--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -16,7 +16,6 @@
 package com.example.android.miwok;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -28,16 +27,22 @@ public class NumbersActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.word_list);
 
         //Creating arrayList of Strings
-        ArrayList<String> words = new ArrayList<String>();
-        words.add("one"); words.add("two"); words.add("three"); words.add("four");
-        words.add("five"); words.add("six"); words.add("seven"); words.add("eight");
-        words.add("nine"); words.add("ten");
+        ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("one","lutti"));
+        words.add(new Word("two","otiikoo"));
+        words.add(new Word("three","tolookosu"));
+        words.add(new Word("four","oyyisa"));
+        words.add(new Word("five","massokka"));
+        words.add(new Word("six","temmokka"));
+        words.add(new Word("seven","kenekaku"));
+        words.add(new Word("eight","kawinta"));
+        words.add(new Word("nine","wo'e"));
+        words.add(new Word("ten","na'aacha"));
 
-        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
-
+        WordAdapter itemsAdapter = new WordAdapter(this, words);
         ListView listview = (ListView) findViewById(R.id.list);
 
         listview.setAdapter(itemsAdapter);
